82e60ddf606fdc41311c3d36fe3e298c795c65f5
harishsurisetty <surisetty988@gmail.com>
2019-04-18 20:57:01 +0530

    Add a Test Sample Project

---
+++ TestSample/src/com/icici/loans/carloan/LoanClass.java
@@ -0,0 +1,11 @@
+package com.icici.loans.carloan;
+
+public class LoanClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the world of selenium");
+	}
+
+}
